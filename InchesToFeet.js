05a90bef614a92d850a5af90b953b07fe0120ad5
sajjadsadiq <sajjadsadiq2020@gmail.com>
2023-12-12 17:53:42 +0600

    inches to feet convert

---
+++ InchesToFeet.js
@@ -0,0 +1,11 @@
+// Function to convert Inches to Feet
+function convertInchesToFeet(inches) {
+  // 1 Foot = 12 Inches
+  const feet = inches / 12;
+  return feet;
+}
+
+// Example Usage
+const inches = 70;
+const feetResult = convertInchesToFeet(inches)
+console.log(`Feet: ${feetResult}`);
